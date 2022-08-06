9a4fa7fcae41eb6cda627d4e6da45b6ee452ab56
ATSUYA TANAKA <a2222at@aiit.ac.jp>
2022-08-06 16:46:13 +0900

    Make the validation function

--- gcfmaker.sh
+++ gcfmaker.sh
@@ -5,23 +5,25 @@
 #  ユークリッドの互除法により、最大公約数を求める。
 #  結果を出力する。
 
+#入力された値の検証関数
+VALIDATION () {
+  if !(expr "$1" : "[0-9]*$" >&/dev/null) || test 0 -eq $1;then
+    echo 自然数を入力してください。 1>&2
+    exit 1
+  fi
+}
+
 echo 最大公約数を求めたい2つの自然数を入力してください.
 
 #1つ目の自然数
 read -p "1つ目の自然数：" DATA1
 #入力された値の検証
-if !(expr "$DATA1" : "[0-9]*$" >&/dev/null) || test 0 -eq $DATA1;then
-  echo 自然数を入力してください. 1>&2
-  exit 1
-fi
+VALIDATION "$DATA1"
 
 #2つ目の自然数
 read -p "2つ目の自然数：" DATA2
 #入力された値の検証
-if !(expr "$DATA2" : "[0-9]*$" >&/dev/null) || test 0 -eq $DATA2;then
-  echo 自然数を入力してください。 1>&2
-  exit 1
-fi
+VALIDATION "$DATA2"
 
 #ユークリッドの互除法
 r=`expr $DATA1 % $DATA2`
