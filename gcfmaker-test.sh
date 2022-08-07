70ee0af4bdcf389ef9b2b026033e0d650eabcb06
ATSUYA TANAKA <a2222at@aiit.ac.jp>
2022-08-07 10:04:31 +0900

    Add test28 to check for abnormal behavior when each input is a double-byte space

--- gcfmaker-test.sh
+++ gcfmaker-test.sh
@@ -155,6 +155,11 @@ echo ERROR：自然数を入力してください。 > ${ans}
 echo -e "81\n　" | ./gcfmaker.sh 1> /dev/null 2> ${result}
 diff ${ans} ${result} || echo テスト27でエラー発生 >> ${err}
 
+#テスト28 異常動作の確認（どちらの入力も全角スペース）
+echo ERROR：自然数を入力してください。 > ${ans}
+echo -e "　\n　" | ./gcfmaker.sh 1> /dev/null 2> ${result}
+diff ${ans} ${result} || echo テスト28でエラー発生 >> ${err}
+
 #エラーの確認
 if [ -f ${err} ]; then
   echo --------------------------------------------------------
